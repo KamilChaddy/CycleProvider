0711b154f90f043a5b356e108b65bece8e364adb
qczauderna <qczauderna@gmail.com>
2019-05-23 15:56:58 +0200

    Dynamic

---
+++ CycleProvider.Library.Tests/CycleProvider.Library.Tests/DynamicUsageTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CycleProvider.Library.Tests
+{
+    [TestClass]
+    public class DynamicUsageTest
+    {
+        [TestMethod]
+        public void DynamicObjectInjection_Demo()
+        {
+            LogFromDynamic(new Person("Dolas") { FirstName = "Franek " });
+            LogFromDynamic(new PrivatePerson { LastName = "PrivatePerson", FirstName = "XXX " });
+            LogFromDynamic(new PrivatePersonValueType { LastName = 123, FirstName = 989 });
+
+            LogFromDynamic(new { FirstName = "Person ", LastName = new {AnyPropety = "dsadsdasda", OtherProperty = "dsadasdasd", } });
+        }
+
+        private static void LogFromDynamic(dynamic fullName)
+        {
+            Console.WriteLine($"FN: {fullName.FirstName}, LN: {fullName.LastName}");
+        }
+        private class PrivatePerson
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        private class PrivatePersonValueType
+        {
+            public int FirstName { get; set; }
+            public int LastName { get; set; }
+        }
+    }
+}
