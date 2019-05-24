b6fed58d720c450b52fc06c256b32bbcc0bdf921
qczauderna <qczauderna@gmail.com>
2019-05-24 13:51:03 +0200

    Last Commit

---
+++ CycleProvider.Library/CycleProvider.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CycleProvider.MVC.Startup))]
+namespace CycleProvider.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
